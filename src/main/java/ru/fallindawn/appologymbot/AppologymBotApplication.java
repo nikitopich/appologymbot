420c7f0e860c52c4b7c63dcddacbbb0bebabd1a0
Никита Бабченко <nvbabchenko@sberinsur.ru>
2021-09-15 22:11:51 +0300

    init commit

---
+++ src/main/java/ru/fallindawn/appologymbot/AppologymBotApplication.java
@@ -0,0 +1,13 @@
+package ru.fallindawn.appologymbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppologymBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppologymBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fallindawn/appologymbot/AppologymBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fallindawn.appologymbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppologymBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
